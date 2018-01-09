ab290f13ddc4f7f7b1a26a0b6fb0a1bd9ac1567f
Pim Meulensteen <pim@meulenste.net>
2018-01-09 19:33:20 +0100

    Updated js

--- js.js
+++ js.js
@@ -12,50 +12,46 @@ factor = Math.round(Math.min(window.innerWidth, window.innerHeight) / 4)
 async function update_canvas() {
     var ctx = ca.getContext("2d");
     ctx.translate(canvas.width / 2, canvas.height / 2);
-
     for (var x = -2 * factor; x < 2 * factor; x++) {
         for (var y = -2 * factor; y < 2 * factor; y++) {
-
             result = algorithm(x / factor, y / factor);
             console.log(algorithm(x / factor, y / factor))
             ctx.fillStyle = 'hsl(' + result + ',100%,50%)';
             ctx.fillRect(x, y, 1, 1);
-
-
         }
-        await sleep(100);
-        console.log('x')
+        await sleep(50);
     }
     ctx.stroke();
 }
 
 
-c = [0.18,-0.72]
+c = [0.18, -0.72]
+
+function update_c() {
+    c = [parseInt(document.getElementById('real_input').value), parseInt(document.getElementById('imag_input').value)];
+}
 
 function algorithm(x, y) {
-    z = [x,y];
+    z = [x, y];
     i = 255;
     while (abs(z) < 2 && i > 0) {
-        z = f(z,c)
+        z = f(z, c)
         i--;
     }
     return i
 
 }
 
-function f(z, c){
-    return [z[0]*z[0] - z[1] * z[1] + c[0], 2 * z[0] * z[1] + c[1]];
+function f(z, c) {
+    return [z[0] * z[0] - z[1] * z[1] + c[0], 2 * z[0] * z[1] + c[1]];
 }
 
-function abs(z){
-    return Math.sqrt(z[0]*z[0] + z[1]*z[1]);
+function abs(z) {
+    return Math.sqrt(z[0] * z[0] + z[1] * z[1]);
 }
 
 
 
-function update_c() {
-    c = [parseInt(document.getElementById('real_input').value), parseInt(document.getElementById('imag_input').value)];
-}
 
 //Resize the window onchange
 function resize() {
