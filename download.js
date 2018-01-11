2caff98c263010bc94159da217a8d59c36feb4a4
Pim Meulensteen <PimMeulensteen@users.noreply.github.com>
2018-01-11 11:48:04 +0100

    Updated some things.
    Responsiveness, fallback character,

--- download.js
+++ download.js
@@ -1,8 +1,7 @@
 var button = document.getElementById('btn-download');
 button.addEventListener('click', function(e) {
     d = new Date()
-    button.setAttribute("download", 'fractal ' + d.getFullYear() + '-' + d.getMonth()+1 + '-' +
-        d.getDate() + ' ' + d.getHours() + '.' + d.getMinutes() + '.' + d.getSeconds())
+    button.setAttribute("download", 'fractal ' + d.getFullYear() + '-' + d.getMonth()+1 + '-' + d.getDate() + ' ' + d.getHours() + '-' + d.getMinutes() + '-' + d.getSeconds()) + '.png';
     var dataURL = ca.toDataURL('image/png');
     button.href = dataURL;
 });
